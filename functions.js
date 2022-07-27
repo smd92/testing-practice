7a682db0a6819a783a93e2c266c343b3a43a2535
smd92 <69214590+smd92@users.noreply.github.com>
2022-07-27 14:30:30 +0200

    added some tests

--- functions.js
+++ functions.js
@@ -8,9 +8,26 @@ function capitalize(string) {
     return string.replace(string[0], string[0].toUpperCase());
 }
 
-console.log(capitalize("asdasd"))
+//takes a string and returns it reversed
+function reverseString(string) {
+  const splitStringArr = string.split("");
+  const reverseArr = splitStringArr.reverse();
+  const revString = reverseArr.join("");
+  return revString;
+}
+
+//contains functions for the basic operations: add, subtract, divide, and multiply
+function Calculator() {
+  this.add = (a, b) => a + b,
+  this.subtract = (a, b) => a - b
+  this.divide = (a, b) => a / b,
+  this.multiply = (a, b) => a * b
+}
+
 
   module.exports = {
     sum,
     capitalize,
+    reverseString,
+    Calculator
   }
--- functions.test.js
+++ functions.test.js
@@ -13,3 +13,28 @@ const capitalize = functions.capitalize;
 test("changes 'hello' to 'Hello'", () => {
   expect(capitalize("hello")).toMatch(/Hello/);
 });
+
+//reverseString test
+const reverseString = functions.reverseString;
+
+test("changes 'hello' to 'olleh'", () => {
+  expect(reverseString("hello")).toMatch(/olleh/);
+});
+
+//calculator test
+const calculator = new functions.Calculator();
+
+test("performs basic mathematical operations for numbers 1, 2", () => {
+  const data = {
+    add: calculator.add(1, 2),
+    subtract: calculator.subtract(1, 2),
+    multiply: calculator.multiply(1, 2),
+    divide: calculator.divide(1, 2),
+  };
+  expect(data).toEqual({
+    add: 3,
+    subtract: -1,
+    multiply: 2,
+    divide: 0.5,
+  });
+});
